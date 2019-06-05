28e8e26b58c10a9f579e808191616b6fdcecf78c
Attew <attew2011r@gmail.com>
2019-06-05 14:08:33 +0200

    first commit

---
+++ example.py
@@ -0,0 +1,10 @@
+def multiply(a, b):
+    return a * b
+    '''
+    this function adds two arguments.
+    '''
+#   return a - b # wont pass the test
+
+def test_add():
+    assert add(2, 3) == 6
+    assert add('space', 'ship') == 'spaceship'
